7fff0b721d53cf74bd7878d03af1224eac36330e
kpa100plus-debug <kpa100plus@gmail.com>
2026-08-21 22:53:21 +0900

    Update data-structure.js

--- js/data-structure.js
+++ js/data-structure.js
@@ -1,1 +1,37 @@
+const NCC_DATA = {
 
+member: {
+name: "",
+gender: "",
+birth: "",
+phone: "",
+email: "",
+address: "",
+interest: "",
+recommender: "",
+agreePrivacy: false,
+agreeMarketing: false
+},
+
+center: {
+name: "",
+phone: "",
+region: "",
+type: "",
+experience: "",
+agree: false
+},
+
+partner: {
+company: "",
+businessType: "",
+representative: "",
+phone: "",
+region: "",
+benefit: "",
+agreement: false
+}
+
+};
+
+console.log("NCC DATA READY");
